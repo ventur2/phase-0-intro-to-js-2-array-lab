0f06c72f06a44dcf187ac1fe3cd06cca9fa26315
rachell <dazzlemevanity@gmail.com>
2022-06-01 19:29:50 -0400

    commit tree ce33997afd972fe983be0f109eb01234c60df54c

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(bob){
+    return cats.push(bob)
+}
+console.log(cats)
+
+function destructivelyPrependCat(mike){
+    return cats.unshift(mike)
+}
+
+function destructivelyRemoveLastCat(garfield){
+    return cats.pop(garfield)
+}
+
+function destructivelyRemoveFirstCat(milo){
+    return cats.shift(milo)
+}
+
+function appendCat(george){
+    // return cats.slice().push(george)
+let copyCats = cats.slice()
+copyCats.push(george)
+return copyCats
+}
+
+function prependCat(frank){
+    let copyCats = cats.slice()
+    copyCats.unshift(frank)
+    return copyCats
+}
+
+function removeLastCat(george){
+    let copyCats = cats.slice()
+    copyCats.pop (george)
+    return copyCats
+}
+
+function removeFirstCat(mike){
+    let copyCats = cats.slice()
+    copyCats.shift(mike)
+    return copyCats
+}
